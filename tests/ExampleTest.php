be8c164cd2edaee8e9a7c1f4f7b8b20fc4f5b480
fasniefjn <144859774+fasniefjn@users.noreply.github.com>
2025-12-01 17:01:37 +0700

    Refactor ExampleTest to include a new test case

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,17 +1,8 @@
 <?php
-
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
-{
-    public function testTambahAngka()
-    {
-        $hasil = 2 + 2;
-        $this->assertEquals(4, $hasil, "Perhitungan salah");
-    }
-
-    public function testStringHello()
-    {
-        $this->assertEquals("hello", strtolower("HELLO"));
+class ExampleTest extends TestCase {
+    public function testTrue() {
+        $this->assertTrue(true);
     }
 }
